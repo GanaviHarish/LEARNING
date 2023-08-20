e7afee63618e05955718c9d3b9037903f6db5a59
GanaviHarish <108727540+GanaviHarish@users.noreply.github.com>
2023-08-20 14:41:37 +0530

    Practice programs

---
+++ Practice programs/predict/src/predict/PredictClass.java
@@ -0,0 +1,14 @@
+package predict;
+
+public class PredictClass {
+
+	public static void grade(int marks) {
+		if(marks>=50)
+		System.out.println("pass");
+		else
+		System.out.println("fail");
+			
+		}
+	}
+
+
